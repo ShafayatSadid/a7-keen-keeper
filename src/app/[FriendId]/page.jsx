311d152b325872c93bdc98b9af8ed319cbae6ffa
ShafayatSadid <mdshafayathossainsadid@gmail.com>
2026-04-15 19:46:08 +0200

    friend details page design complete.

--- src/app/[FriendId]/page.jsx
+++ src/app/[FriendId]/page.jsx
@@ -1,6 +1,9 @@
 import friends from '@/data/friendsData.json';
 import Image from 'next/image';
 import { FiArchive } from 'react-icons/fi';
+import { IoVideocamOutline } from 'react-icons/io5';
+import { LuPhoneCall } from 'react-icons/lu';
+import { MdOutlineTextsms } from 'react-icons/md';
 import { RiDeleteBin6Line, RiNotificationSnoozeLine } from 'react-icons/ri';
 
 const FriendDetailsPage = async ({ params }) => {
@@ -11,13 +14,14 @@ const FriendDetailsPage = async ({ params }) => {
     const { id, name, picture, email, days_since_contact, status, tags, bio, goal, next_due_date } = targetFriend;
 
     return (
-        <div className='my-20'>
+        <div className='my-20 flex flex-wrap justify-center gap-6'>
+
             {/* left side */}
             <div className=''>
 
 
                 {/* profile and bio */}
-                <div className='py-6 max-w-[350px] shadow-sm rounded-lg bg-[#ffffff] mx-auto'>
+                <div className='py-6 w-[350px] shadow-sm rounded-lg bg-[#ffffff] mx-auto'>
                     {/* card image */}
                     <div className='rounded-full'>
                         <Image src={picture}
@@ -45,26 +49,97 @@ const FriendDetailsPage = async ({ params }) => {
 
                         <p className='text-[1rem] text-center my-3 text-[#64748B] italic max-w-[254px] mx-auto font-medium'>"{bio}"</p>
 
-                        <p className='text-[#64748B] font-medium text-[0.750rem] text-center '>Preferred: email</p>
+                        <p className='text-[#64748B] text-[0.875rem] text-center '>Preferred: email</p>
                     </div>
                 </div>
 
                 {/* snooze, archive, delete */}
-                <div className='flex justify-center items-center gap-2 max-w-[350px] h-[53px] shadow-sm rounded-lg bg-[#ffffff] mt-4 text-[1rem] font-medium text-[#1F2937]/80 mx-auto'>
+                <div className='flex justify-center items-center gap-2 w-[350px] h-[53px] shadow-sm rounded-lg bg-[#ffffff] mt-4 text-[1rem] font-medium text-[#1F2937]/80 mx-auto'>
                     <span><RiNotificationSnoozeLine /></span>
                     <p className=' '>Snooze 2 weeks</p>
                 </div>
 
                 {/* archive */}
-                <div className='flex justify-center items-center gap-2 max-w-[350px] h-[53px] shadow-sm rounded-lg bg-[#ffffff] mt-4 text-[1rem] font-medium text-[#1F2937]/80 mx-auto'>
+                <div className='flex justify-center items-center gap-2 w-[350px] h-[53px] shadow-sm rounded-lg bg-[#ffffff] mt-4 text-[1rem] font-medium text-[#1F2937]/80 mx-auto'>
                     <span><FiArchive /></span>
                     <p className=' '>Archive</p>
                 </div>
 
                 {/* delete */}
-                <div className='flex justify-center items-center gap-2 max-w-[350px] h-[53px] shadow-sm rounded-lg bg-[#ffffff] mt-4 text-[1rem] font-medium text-[#EF4444]/80 mx-auto'>
+                <div className='flex justify-center items-center gap-2 w-[350px] h-[53px] shadow-sm rounded-lg bg-[#ffffff] mt-4 text-[1rem] font-medium text-[#EF4444]/80 mx-auto'>
                     <span><RiDeleteBin6Line /></span>
                     <p className=' '>Delete</p>
+                </div>
+            </div>
+
+            {/* right side */}
+            <div className='px-2'>
+                {/* tracking history */}
+                <div className='flex flex-wrap items-center gap-6 justify-center'>
+                    {/* one */}
+                    <div className='w-[229px] h-[134px] bg-[#ffffff] shadow-sm rounded-lg flex flex-col justify-center items-center gap-2'>
+                        <h2 className='text-[1.875rem] font-semibold text-[#244D3F]'>{days_since_contact}</h2>
+
+                        <p className='text-[1.125rem] text-[#64748B]'>Days Since Contact</p>
+                    </div>
+
+                    {/* two */}
+                    <div className='w-[229px] h-[134px] bg-[#ffffff] shadow-sm rounded-lg flex flex-col justify-center items-center gap-2'>
+                        <h2 className='text-[1.875rem] font-semibold text-[#244D3F]'>{goal}</h2>
+
+                        <p className='text-[1.125rem] text-[#64748B]'>Goal (Days)</p>
+                    </div>
+
+                    {/* three */}
+                    <div className='w-[229px] h-[134px] bg-[#ffffff] shadow-sm rounded-lg flex flex-col justify-center items-center gap-2'>
+                        <h2 className='text-[1.875rem] font-semibold text-[#244D3F]'>{next_due_date}</h2>
+
+                        <p className='text-[1.125rem] text-[#64748B]'>Next Due</p>
+                    </div>
+                </div>
+
+                {/* Relationship goal */}
+                <div className='max-w-[736px] h-[121px] bg-[#ffffff] shadow-sm rounded-lg mt-6 p-6'>
+                    <div className='flex items-center justify-between'>
+                        <h2 className='text-[1.25rem] font-medium text-[#244D3F] '>Relationship Goal</h2>
+                        <button className='btn text-[0.785rem] font-medium '>Edit</button>
+                    </div>
+
+                    <p className='text-[1rem] text-[#64748B] mt-3'>Connect every <span className='font-bold text-[#1F2937] '>{goal} days</span></p>
+                </div>
+
+                {/* Quick check */}
+                <div className='max-w-[736px] bg-[#ffffff] shadow-sm mt-6 rounded-lg p-6'>
+
+                    <h2 className='text-[1.25rem] font-medium text-[#244D3F] text-center md:text-left'>Quick Check-In</h2>
+
+                    {/* call text */}
+                    <div className='mt-4 flex flex-wrap items-center justify-center gap-4'>
+                        {/* call */}
+                        <div className='w-[218px] h-[95px] btn flex flex-col gap-2 text-[1.125rem] '>
+                            <span>
+                                <LuPhoneCall />
+                            </span>
+                            <p>Call</p>
+                        </div>
+
+                        {/* text */}
+                        <div className='w-[218px] h-[95px] btn flex flex-col gap-2 text-[1.125rem] '>
+                            <span>
+                                <MdOutlineTextsms />
+                            </span>
+                            <p>Text</p>
+                        </div>
+
+
+                        {/* video */}
+                        <div className='w-[218px] h-[95px] btn flex flex-col gap-2 text-[1.125rem] '>
+                            <span>
+                                <IoVideocamOutline />
+                            </span>
+                            <p>Video</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
